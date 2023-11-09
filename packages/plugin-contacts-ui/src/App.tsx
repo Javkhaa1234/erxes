7b6137af58acd2811e1a66afceaa20115613f7ab
Dulguun Otgon <dulguun0225@gmail.com>
2023-11-09 21:20:16 +0800

    init

--- packages/plugin-contacts-ui/src/App.tsx
+++ packages/plugin-contacts-ui/src/App.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import GeneralRoutes from "./generalRoutes";
 import { PluginLayout } from "@erxes/ui/src/styles/main";
 import { AppProvider } from "coreui/appContext";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "@erxes/ui/src/apolloClient";
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+dayjs.extend(localizedFormat);
 
 const App = () => {
   return (
-    <AppProvider>
-      <PluginLayout>
-        <GeneralRoutes />
-      </PluginLayout>
-    </AppProvider>
+    <ApolloProvider client={apolloClient}>
+      <AppProvider>
+        <PluginLayout>
+          <GeneralRoutes />
+        </PluginLayout>
+      </AppProvider>
+    </ApolloProvider>
   );
 };
 
