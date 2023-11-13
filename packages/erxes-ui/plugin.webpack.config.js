fd82cce0ba58174dbe8007c4d7581f0841056561
Dulguun Otgon <dulguun0225@gmail.com>
2023-11-13 10:19:20 +0800

    remove unnecessary

--- packages/erxes-ui/plugin.webpack.config.js
+++ packages/erxes-ui/plugin.webpack.config.js
@@ -201,11 +201,7 @@ module.exports = (configs) => (env, args) => {
           "@erxes/ui": {
             requiredVersion: "1.0.0",
             singleton: true,
-          },
-          dayjs: {
-            requiredVersion: deps["dayjs"],
-            singleton: true,
-          },
+          }
         },
       }),
       new HtmlWebPackPlugin({
